bfa519f8efb9d7e3b42b6933162de317aa7632b1
Mohammed-Saif0 <mohammedsaif9513@gmail.com>
2023-03-07 20:51:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+import utils from './utils/utils.js';
+
+const searchButton = document.querySelector('#query-button');
+const searchInput = document.querySelector('#query-input');
+const weatherCard = document.querySelector('.todays-weather');
+const showMoreButton = document.querySelector("#more-weather-btn");
+const showLessButton = document.querySelector("#show-less-btn");
+const weatherSection = document.querySelector(".weather-section");
+
+const displaypreviousWeather = (data) => {
+    utils.checkForSearch(weatherCard, weatherSection);
+    weatherCard.innerHTML = utils.weatherCardElement(data.days[0], data.resolvedAddress)
+    weatherCard.style.display = "block";
+    showMoreButton.style.display = "block";
+    weatherCard.style.display = "block";
+    searchButton.disabled = true;
+}
+const displayTodaysWeather = (data) => {
+    utils.checkForSearch(weatherCard, weatherSection);
+    weatherCard.innerHTML = utils.weatherCardElement(data.days[0], data.resolvedAddress)
+
+    localStorage.setItem('location', data.address.slice(0, -1));
+    weatherCard.style.display = "block";
+    showMoreButton.style.display = "block";
+    weatherCard.style.display = "block";
+
+    searchButton.disabled = true;
+}
+
+const displayMoreWeathers = (data) => {
+
+    let weatherSection = document.querySelector(".weather-section");
+    console.log(data);
+
+    for (let i = 1; i <= 5; i++) {
+        let eachWeather = document.createElement("div");
+        eachWeather.setAttribute('class', 'weather-card');
+        eachWeather.innerHTML = utils.weatherCardElement(data.days[i], data.resolvedAddress)
+        weatherSection.append(eachWeather)
+    }
+
+    showMoreButton.style.display = "none";
+    showLessButton.style.display = "block";
+
+}
+
+showLessButton.addEventListener('click', (e) => {
+    let arr = weatherSection.children
+    Array.from(arr).forEach(x => {
+        if (x.tagName === 'DIV' && x.id !== 'todays-weather')
+            weatherSection.removeChild(x)
+    })
+    showLessButton.style.display = "none";
+    showMoreButton.style.display = "block";
+})
+
+searchButton.disabled = true;
+// checking of input feild is not empty
+searchInput.onkeyup = () => {
+    if (searchInput.value.length > 0) {
+        searchButton.disabled = false;
+    } else {
+        searchButton.disabled = true;
+    }
+}
+
+weatherCard.style.display = "none"
+
+if (localStorage.getItem('location')) {
+    utils.fetchWeather("", displaypreviousWeather);
+}
+
+
+utils.queryLocation(searchButton, searchInput, displayTodaysWeather);
+utils.queryLocation(showMoreButton, "", displayMoreWeathers)
---
+++ utils/utils.js
@@ -0,0 +1,92 @@
+
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) * (5 / 9);
+}
+
+const fetchWeather = async (inputElement, callback) => {
+    let query = '';
+
+    if (typeof (inputElement) === "object") {
+        query = inputElement.value;
+        // clearing the input feild 
+        inputElement.value = "";
+    }
+    else {
+        query = localStorage.getItem('location')
+    }
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}%7D?unitGroup=us&key=HEG4F8F8KWRVJJMYWBWT267MR&contentType=json`;
+
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        callback(data);
+
+    }
+    catch (error) {
+        console.log(error);
+
+        alert(`${query} location not found`);
+    }
+    
+}
+ 
+//Function to get display (on divElement) weather whenever the buttonElement is clicked 
+const queryLocation = (buttonElement, inputElement, callback) => {
+    buttonElement.addEventListener('click', function () {
+        fetchWeather(inputElement, callback); 
+        // buttonElement.disabled = true;
+    });
+}
+
+const checkForSearch = (weatherCard,weatherSection) => {
+    if (weatherCard.innerText !== '') {
+        let arr = weatherSection.children
+        Array.from(arr).forEach(x => {
+            if ((x.tagName === 'DIV' && x.id !== 'todays-weather'))
+                weatherSection.removeChild(x);
+            if (x.id === 'todays-weather')
+                x.innerText = "";
+            if (x.id === 'more-weather-btn') {
+                x.style.display = "none";
+            }
+            if (x.id === 'show-less-btn') {
+                x.style.display = "none";
+            }
+        })
+    }
+}
+
+const weatherCardElement = (data,address) =>{
+    let date = data.datetime.slice(-2);
+    let d = new Date();
+    let datetime = new Date(data.datetime).toDateString()
+    if(date == d.getDate())
+    datetime = "Today";
+    else if(date-1 == d.getDate())
+    datetime= "Tomorrow";
+    let a =  `
+    <div id="date">
+        <span class="material-icons md-60   ">calendar_today</span> :<span> ${datetime} </span>
+    </div>
+    <div id="location">
+        <span class="material-icons md-60" >location_on  </span> : <span>${address}</span> 
+    </div> 
+    <div id="temp"> 
+        <span class="material-icons md-60">device_thermostat</span>:${fahrenheitToCelsius(data.tempmax).toFixed(2)}  Degree Celcius 
+    </div>
+    <div id="desc">
+        <span class="material-icons md-60">description</span> ${data.description}
+    </div>
+    ` ;
+
+    return a;
+}
+let obj = {
+    queryLocation: queryLocation,
+    fahrenheitToCelsius: fahrenheitToCelsius,
+    checkForSearch:checkForSearch,
+    weatherCardElement:weatherCardElement,
+    fetchWeather:fetchWeather
+};
+
+export default obj;
